d02d7ca8785de574998125e141b538b768d4a9b7
Ettalha0x <31565378+ettalha0x@users.noreply.github.com>
2026-04-29 19:19:27 +0100

    Update MariaDB user creation to allow remote access

--- srcs/requirements/mariadb/conf/config.sh
+++ srcs/requirements/mariadb/conf/config.sh
@@ -4,8 +4,8 @@ echo "
 FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWD}';
 CREATE DATABASE  IF NOT EXISTS  ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER  IF NOT EXISTS  '${DB_USER}'@'localhost' IDENTIFIED by '${DB_PASSWD}';
-GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'localhost';
+CREATE USER  IF NOT EXISTS  '${DB_USER}'@'%' IDENTIFIED by '${DB_PASSWD}';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;" > init_db.sql
 
 mariadbd --user=mysql --bootstrap < /init_db.sql
